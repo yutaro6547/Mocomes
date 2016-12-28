709ae342a97a63cf7f94174bf926b22b69713350
yutaro <yutaro@shizuru.local>
2016-12-28 21:14:36 +0900

    Create New messenger App named “Mocomes”.

---
+++ Mocomes/Mocomes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mocomes
+//
+//  Created by 鈴木裕太朗 on 2016/12/28.
+//  Copyright © 2016年 鈴木裕太朗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
