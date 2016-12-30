eb413532061632a37dc54c9aca067d4743529b15
yutaro <yutaro@shizuru.local>
2016-12-30 21:23:30 +0900

    Set Bar Item.

--- Mocomes/Mocomes/MenuScreen.swift
+++ Mocomes/Mocomes/MenuScreen.swift
@@ -2,7 +2,7 @@
 //  MenuScreen.swift
 //  Mocomes
 //
-//  Created by 鈴木裕太朗 on 2016/12/29.
+//  Created by ytzuki on 2016/12/29.
 //  Copyright © 2016年 ytzuki. All rights reserved.
 //
 
@@ -12,24 +12,9 @@ class MenuScreen: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
