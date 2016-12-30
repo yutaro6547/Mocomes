5fb362b0ecd2f22616b2ac22e67eba598fed8977
yutaro <yutaro@shizuru.local>
2016-12-30 23:19:07 +0900

    Create Talk Screen to implement a messenger function.

---
+++ Mocomes/Mocomes/TalkScreen.swift
@@ -0,0 +1,35 @@
+//
+//  TalkScreen.swift
+//  Mocomes
+//
+//  Created by ytzuki on 2016/12/30.
+//  Copyright © 2016年 ytzuki. All rights reserved.
+//
+
+import UIKit
+
+class TalkScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
