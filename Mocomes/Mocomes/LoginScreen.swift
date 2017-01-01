5959e1573c1393b0bcac3990978cc5379164b844
yutaro <yutaro@shizuru.local>
2017-01-01 22:58:05 +0900

    Implemented a login screen.

--- Mocomes/Mocomes/LoginScreen.swift
+++ Mocomes/Mocomes/LoginScreen.swift
@@ -9,12 +9,19 @@
 import UIKit
 import FirebaseAuth
 
-class LoginScreen: UIViewController {
+class LoginScreen: UIViewController, UITextFieldDelegate {
   @IBOutlet var emailLogin: UITextField!
   @IBOutlet var passwordLogin: UITextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    emailLogin.delegate = self
+    passwordLogin.delegate = self
+    passwordLogin.isSecureTextEntry = true
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
   }
 
   override func didReceiveMemoryWarning() {
@@ -22,6 +29,36 @@ class LoginScreen: UIViewController {
   }
   
   @IBAction func loginButton() {
+    login()
   }
 
+  func login() {
+    //EmailとPasswordのTextFieldに文字がなければ、その後の処理をしない
+    guard let email = emailLogin.text else { return }
+    guard let password = passwordLogin.text else { return }
+
+    //signInWithEmailでログイン
+    //第一引数にEmail、第二引数にパスワードを取ります
+    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+      //エラーがないことを確認
+      if error == nil {
+        if user != nil {
+          self.transitionToView()
+        }
+      }else {
+        print("error...\(error?.localizedDescription)")
+      }
+    })
+  }
+
+  //Returnキーを押すと、キーボードを隠す
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+
+  //ログイン完了後に、ListViewControllerへの遷移のためのメソッド
+  func transitionToView()  {
+    self.performSegue(withIdentifier: "toVC", sender: self)
+  }
 }
